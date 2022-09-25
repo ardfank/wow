27cad8046c1a4e7008603057b2b46e22a792ae91
ardfank <hitoriki.batosai@gmail.com>
2022-09-26 02:11:35 +0800

    up ads

--- index.php
+++ index.php
@@ -359,7 +359,8 @@ function drop(e) {
 			if (xhr.status == 200) {
 				im.push("foto\/"+dt);
 				$('#gal').prepend("<div class='responsive' alt='"+dt+"' title='"+dt+"'><img class='wow' loading='lazy' src='foto/"+dt+"' index='"+(im.length-1)+"' onclick='javascript:light("+(im.length-1)+");'></div>");
-				$('#up').toggle(200).html("");
+				// $('#up').toggle(200).html("");
+				$('#up,#ads').toggle(200);
 			}
 		};
 		xhr.send(formData);
@@ -391,7 +392,8 @@ function drop(e) {
 					$.post(window.location, {url: imgs,name: dt}, function() {
 						im.push("foto\/"+dt);
 						$('#gal').prepend("<div class='responsive' alt='"+dt+"' title='"+dt+"'><img class='wow' loading='lazy' src='foto/"+dt+"' index='"+(im.length-1)+"' onclick='javascript:light("+(im.length-1)+");'></div>");
-						$('#up').toggle(200).html("");
+						// $('#up').toggle(200).html("");
+						$('#up,#ads').toggle(200);
 					});
 				}
 			}
